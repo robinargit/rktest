8d5522252a6e5f8f510e9d113c5306aafebda0a8
robinargit <robinargit@gmail.com>
2022-11-25 22:50:13 +0600

    my 1st commit

---
+++ src/main/java/com/example/rktest/RktestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RktestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rktest/RktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RktestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
